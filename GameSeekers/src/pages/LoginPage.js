de5a27c4018b21c8bbee4bccb5aa456e9819e7cd
MiloszDziadosz <milosz.dziadosz1997@gmail.com>
2022-05-22 23:36:50 +0200

    update

--- GameSeekers/src/pages/LoginPage.js
+++ GameSeekers/src/pages/LoginPage.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import {login} from '../actions/auth';
 
-const LoginPage = () => {
-    return (
-        <div>
-            <label htmlFor="">Podaj login<input type="text" /></label>
-            <label htmlFor="">Podaj hasło<input type="password" /></label>
-            <button>Zaloguj</button>
-        </div>
-    );
+class LoginPage extends React.Component {
+    
+    constructor(props){
+        super(props)
+        this.state={
+            username: "",
+            password: "",
+        };
+    }
+
+    changeValue = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    submit = () => {login(this.state.username, this.state.password)}
+   
+    render(){    
+        return (
+            <div>
+                <input
+                    name="username"
+                    type="text"
+                    value={this.state.username}
+                    onChange={this.changeValue}
+                    placeholder = "username"
+                /> 
+                <input
+                    name="password"
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.changeValue}
+                    placeholder = "password"
+                />
+                <button onClick={this.submit}>login</button> 
+            </div>
+        );
+    }
 }
 
 export default LoginPage;
