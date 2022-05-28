dd05a3990a791a673c7ae79a8a08b27d88f5c39f
Alekseyenko <kristina.alekseyenko@nokia.com>
2022-05-28 13:36:37 +0200

    room creation form, TODO style

--- GameSeekers/src/pages/CreateRoomPage.js
+++ GameSeekers/src/pages/CreateRoomPage.js
@@ -4,21 +4,50 @@ import { Prompt } from 'react-router-dom';
 
 
 class CreateRoomPage extends React.Component {
-    state = {
-        name: "",
-        gamename: "",
-        desc: "",
-        isEmpty: true,
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            room_name: "",
+            gamename: "",
+            maxsize: "",
+            // isEmpty: true,
+        }
     }
+  
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.setState({
-            name: "",
-            gamename: "",
-            desc: "",
-            isEmpty: true
-        })
+        console.log(JSON.stringify({
+            "room_name": this.state.room_name,
+            "maxsize": this.state.maxsize,
+            "members": [{ username: localStorage.getItem('currentUser') }]
+        }))
+        try {
+            let res = fetch(
+                "https://game-seekers-backend.herokuapp.com/v1/room/", {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+                },
+                body:
+                    JSON.stringify({
+                        "room_name": this.state.room_name,
+                        "maxsize": this.state.maxsize,
+                        "members": [{ username: localStorage.getItem('currentUser') }]
+                    })
+
+            })
+                // .then((res) => res.json())
+            if (res.status === 200) {
+                console.log(res)
+            } else {
+                console.log(res)
+            }
+        } catch (err) {
+            console.log(err.message)
+        }
     }
 
 
@@ -28,16 +57,16 @@ class CreateRoomPage extends React.Component {
         });
     }
 
-    
+
 
     render() {
         return (
             <div className="contact">
                 <form onSubmit={this.handleSubmit}>
                     <h3>Tworzenie pokoju</h3>
-                    <textarea  onChange={this.handleChange} placeholder="Nazwa pokoju"></textarea>
-                    <textarea  onChange={this.handleChange} placeholder="Nazwa gry"></textarea>
-                    <textarea  onChange={this.handleChange} placeholder="Opis..."></textarea>
+                    <input name="room_name" type="text" value={this.state.value} onChange={this.handleChange} placeholder="Nazwa pokoju"></input>
+                    <input name="game" type="text" value={this.state.value} onChange={this.handleChange} placeholder="Gra"></input>
+                    <input name="maxsize" type="number" value={this.state.value} onChange={this.handleChange} placeholder="Maksymalna liczba graczy"></input>
                     <button onClick={this.handleSubmit}>Utwórz</button>
                 </form>
                 <Prompt
